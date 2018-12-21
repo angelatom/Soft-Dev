40b6d6c71d42b242a87cb735a004c88632cbec56
Angela Tom <atom@stuy.edu>
2018-12-21 15:47:32 -0500

    Update dommy.js

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -5,11 +5,13 @@ K30 -- Sequential Progression III: Season of the Witch
 2018-12-21
 */
 
+//changes heading to match "this" which is what the mouse is hovering over
 var changeHeading = function(e) {
     var h = document.getElementById("h")
     h.innerHTML = this.innerHTML
 };
 
+//default heading for when mouse is not over an item
 var setHeading = function(e) {
     var h = document.getElementById("h");
     h.innerHTML = "Hello World!";
@@ -17,20 +19,24 @@ var setHeading = function(e) {
 
 var lis = document.getElementsByTagName("li");
 
+//removes "this" which is the item that mouse is hovering over
 var removeItem = function(e) {
     this.remove();
 };
 
+//goes through list and gives it the remove/change heading functionality
 for(var i=0; i < lis.length; i++) {
     lis[i].addEventListener('mouseover', changeHeading);
     lis[i].addEventListener('mouseout', setHeading);
     lis[i].addEventListener('click', removeItem);
 };
 
+//adds an entry containing "word" to the list
 var addItem = function(e) {
     var list = document.getElementById("thelist");
     var item = document.createElement("li");
     item.innerHTML = "WORD";
+    //updates item with the functionalities of the other parts of the list
     item.addEventListener('mouseover', changeHeading);
     item.addEventListener('mouseout', setHeading);
     item.addEventListener('click', removeItem);
@@ -40,6 +46,7 @@ var addItem = function(e) {
 var button = document.getElementById("b");
 button.addEventListener('click', addItem);
 
+//fxn for getting nth num in fib sequence
 var fib = function(n) {
     if(n < 2){
         return 1;
@@ -48,12 +55,17 @@ var fib = function(n) {
     }
 };
 
+//for numbering the fib sequence numbers
 var counter = 0;
 var addFib = function(e){
     console.log(e);
+
+    //retrieves the list that we will be adding to soon, and creates an element for each entry in lis
     var list = document.getElementById("fiblist");
     var item = document.createElement("li");
     //item.setAttribute("fib", );
+
+    //edits the entry to hold the fib numbers and adds it to the list we retrieved
     item.innerHTML = fib(counter);
     list.appendChild(item);
     counter = counter + 1;
